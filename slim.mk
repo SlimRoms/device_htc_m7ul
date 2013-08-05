50a4c925b36e6048363bb50276d89480e2f9a2c3
Nicholas Flintham <nick@flinny.org>
2013-08-05 15:09:57 +0100

    Use hdpi bootanim for now
    Change-Id: I1ffa596c5407d151aaccea99c3f7ae5d5f3c10da

--- slim.mk
+++ slim.mk
@@ -7,6 +7,10 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
+#bootanimation
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="htc_europe/m7/m7:4.1.2/JZO54K/177101.12:user/release-keys" PRIVATE_BUILD_DESC="1.29.401.12 CL177101 release-keys"
 
 PRODUCT_NAME := slim_m7ul
