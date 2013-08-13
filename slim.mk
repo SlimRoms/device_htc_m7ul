e516d50d2bcf7734d5e9d684e29e394986b6c891
Lars Greiss <kufikugel@googlemail.com>
2013-08-13 12:56:32 +0200

    Adjust slim.mk for new bootanimation
    This reverts commit 50a4c925b36e6048363bb50276d89480e2f9a2c3.
    
    Change-Id: I90f9e70f1e7d9da5f0d6a7dc47ecd35e047f00a0

--- slim.mk
+++ slim.mk
@@ -7,10 +7,6 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-#bootanimation
-PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JWR66V.H10 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66V.H10/230993:user/release-keys" PRIVATE_BUILD_DESC="3.06.1700.10 CL230993 release-keys"
 
 PRODUCT_NAME := slim_m7ul
